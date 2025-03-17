6e10bb515c1f3c3d778003ab6969e59fc1bd8815
Artur <artur.kuzel@gmail.com>
2025-03-17 16:10:03 +0100

    player movement animation

--- PlayerTest/Assets/Scripts/PlayerScript.cs
+++ PlayerTest/Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,18 @@
+using System;
 using UnityEngine;
 
-public class PlayerScript1 : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
+    public Animator animator;
+    private bool isFacingRight = true;
+
     private float horizontal;
     private float vertical;
     private float speed = 4.0f;
     private Rigidbody2D rb;
     void Start()
     {
-        rb= GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,7 +20,24 @@ public class PlayerScript1 : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        
-        rb.linearVelocity = new Vector2 (horizontal*speed, vertical*speed);
+        animator = GetComponent<Animator>();
+        rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+        FlipSprite();
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        animator.SetFloat("Velocity",Math.Abs(rb.linearVelocity.magnitude));
+    }
+
+    void FlipSprite()
+    {
+        if (horizontal > 0 && !isFacingRight || horizontal < 0 && isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
+        }
     }
 }
