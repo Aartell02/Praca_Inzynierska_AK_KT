2a9a75441cbd4962fa746f7574bab31eb6c04fc7
Artur <artur.kuzel@gmail.com>
2025-09-06 00:23:40 +0200

    assemblies

---
+++ Praca_Inzynierska_AK_KT/Assets/Scripts/DOTS/Systems/Player/PlayerInputSystem.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using UnityEngine;
+using Core.Services;
+
+
+// Input Handling
+namespace DOTS
+{
+	partial class PlayerSystem : SystemBase
+	{
+		internal readonly IPlayerInputService _playerInput;
+		protected override void OnCreate()
+		{
+
+		}
+		protected override void OnUpdate()
+		{
+
+		}
+
+	}
+}
