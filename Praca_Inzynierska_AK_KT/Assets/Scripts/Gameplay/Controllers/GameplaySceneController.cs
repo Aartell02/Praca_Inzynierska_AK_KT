1bbf2bbb1d7278be828d81fa92e0122c75ea7951
Artur <artur.kuzel@gmail.com>
2025-11-20 18:27:52 +0100

    behaviortree

--- Praca_Inzynierska_AK_KT/Assets/Scripts/Gameplay/Controllers/GameplaySceneController.cs
+++ Praca_Inzynierska_AK_KT/Assets/Scripts/Gameplay/Controllers/GameplaySceneController.cs
@@ -1,9 +1,37 @@
+using Core;
 using UnityEngine;
 
 namespace Gameplay.Controllers
 {
 	public class GameplaySceneController : MonoBehaviour
 	{
+		GameObjectReferences gameObjectReferences;
+		EnemySpawnConfig enemySpawnConfig;
+		public void Awake()
+		{
+			gameObjectReferences = GameObjectReferences.Instance;
+			enemySpawnConfig = ConfigReferences.Instance.enemyConfig;
+			SpawnPlayer();
+			SpawnEnemies();
+		}
 
+		public void SpawnPlayer()
+		{
+			GameObject playerPrefab = gameObjectReferences.Player;
+
+			GameObject player = Instantiate(playerPrefab, Vector2.zero, Quaternion.identity);
+		}
+		public void SpawnEnemies()
+		{
+			var enemies = enemySpawnConfig.EnemySpawnData;
+			for (int i = 0; i< enemies.Length; i++)
+			{
+				GameObject enemyPrefab = gameObjectReferences.Enemy[i];
+				for (int j = 0; j < enemies[i].Count; j++)
+				{
+					GameObject enemy = Instantiate(enemyPrefab, Vector2.zero + new Vector2(10,j), Quaternion.identity);
+				}
+			}
+		}
 	}
 }
