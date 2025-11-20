6b95d0e2c3c7a1a999df5d17427c2b052535ce0a
Artur <artur.kuzel@gmail.com>
2025-11-20 15:16:48 +0100

    clnp

---
+++ Praca_Inzynierska_AK_KT/Assets/Scripts/GameSystems/Enemies/AI/EnemyBrain.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace GameSystems
+{
+    class EnemyBrain : MonoBehaviour
+    {
+		AIEnemyState currentState;
+		AIEnemyState previousState;
+		AIEnemyState requestedState;
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Praca_Inzynierska_AK_KT/Assets/Scripts/GameSystems/Enums.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace GameSystems
+{
+	public enum AIEnemyState
+	{
+		Wait,
+		Roam,
+		Scout,
+		Guard,
+		Defend,
+		Attack
+	}
+}
